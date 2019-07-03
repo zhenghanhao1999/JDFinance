e67b0149d25898200c156327bffe5b4d8939875b
郑浩瀚 <571764470@qq.com>
2019-07-03 17:23:02 +0800

    启动项目至完成首页新手特权

---
+++ app/js/core/slider.vue
@@ -0,0 +1,53 @@
+<template>
+    <section :class="cname">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link :to="{name:item.href}">
+                    <img :src="item.src" alt="">
+                </router-link>
+            </swiper-slide>
+            <div v-if="options.pagination" slot="pagination" class="swiper-pagination"/>
+        </swiper>
+    </section>
+</template>
+<script>
+// eslint-disable-next-line node/no-extraneous-import
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+    components:{
+        swiper,
+        swiperSlide
+    },
+    props:{
+        cname:{
+            type:String,
+            default:''
+        },
+        options:{
+            type:Object,
+            default(){
+                return{
+                    autoplay: true,//自动播放
+                    loop:true,//循环
+                    pagination: {
+                        el:'.swiper-pagination'//指示器
+                    },
+                    notNextTick:false
+                }
+            }
+        },
+        items:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '~swiper/dist/css/swiper.css';//~让loader能够找到文件
+
+</style>
---
+++ app/js/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="$style.home">
+        <heador/>
+        <slider/>
+        <novice/>
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header.vue'
+import Slider from './hslider.vue'
+import novice from './novice.vue'
+
+export default {
+    components:{
+        Heador,
+        Slider,
+        novice
+    }
+}
+</script>
+
+<style lang="scss" module>
+  
+</style>
